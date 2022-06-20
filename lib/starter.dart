a61bc969a12b56c210a48c2fbb5f55f2c7777973
Rafael Kenji Nagai <rafakenji23@gmail.com>
2022-06-20 12:02:12 -0300

    feat: map was created - 1#

--- lib/starter.dart
+++ lib/starter.dart
@@ -1,9 +1,6 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:lesson_flutter_bonfire/player/player_controller.dart';
 
-import 'enemy/enemy_controller.dart';
 import 'main.dart';
 
 class Starter extends StatefulWidget {
@@ -16,48 +13,11 @@ class Starter extends StatefulWidget {
 class _StarterState extends State<Starter> {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.landscapeRight,
-    ]);
     return BonfireTiledWidget(
-      joystick: Joystick(
-        keyboardConfig: KeyboardConfig(
-          keyboardDirectionalType: KeyboardDirectionalType.wasdAndArrows,
-        ),
-        directional: JoystickDirectional(
-          color: Colors.orange,
-        ),
-        actions: [
-          JoystickAction(
-            actionId: 1,
-            color: Colors.orange,
-            margin: const EdgeInsets.all(40),
-          ),
-        ],
-      ),
       map: TiledWorldMap(
         'maps/map.json',
-        forceTileSize: const Size(48, 48),
+        forceTileSize: const Size(tileSize, tileSize),
       ),
-      player: PlayerController(
-        Vector2(tileSize * 17, tileSize * 10),
-      ),
-      cameraConfig: CameraConfig(
-        moveOnlyMapArea: true,
-        zoom: 1.5,
-        smoothCameraEnabled: true,
-        sizeMovementWindow: Vector2(
-          tileSize * 2,
-          tileSize * 2,
-        ),
-      ),
-      showCollisionArea: false,
-      components: [
-        EnemyController(Vector2(tileSize * 5, tileSize * 5)),
-        EnemyController(Vector2(tileSize * 18, tileSize * 18)),
-        EnemyController(Vector2(tileSize * 18, tileSize * 5)),
-      ],
     );
   }
 }
